eecd38ddcf2e6b6bc3bb8ab569fe62f1251e22e5
rumikoacopa <rumikorios@gmail.com>
2021-02-19 10:16:34 -0500

    commit tree 87bddedfb0742a62635d7e4701690e054606934d

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,30 @@
 // Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    //defining initial state:
+    this.state = {
+      toggled: false
+    };
+  }
+
+      //update our state here
+      // when handleClick is called, setState will update the state so that toggle is reversed
+  handleClick = () => {
+    this.setState({
+      hasBeenClicked: true
+    }, () => console.log(this.state.hasBeenClicked)) // prints true
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+      </div>
+    );
+  }  
+}
+export default ClickityClick;
